81bed0196b503fa6ca8fd616c5661b61473810ad
Hilina_Beyene <beyenehlina@gmail.com>
2019-05-16 01:41:29 -0400

    updated app.component and app.module

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { IsVisibleDirective } from './Homework_13/isVisible.directive';
 
 @Component({
   selector: 'app-root',
   //templateUrl: './app.component.html',
   template: `
     <div>
+      <h1>Homework_12</h1>
       <counter1 [counter]="counterValueOne" (counterChange)="onChangeOne($event)"></counter1>
       <p>
         <span>Component1 Counter Value = {{componentCounterValueOne}} </span>
@@ -16,11 +18,34 @@ import { Component } from '@angular/core';
         <span>Component2 Counter Value = {{componentCounterValueTwo}} </span>
       </p>
     </div>
+    <br> <br>
+    <div>
+      <h1>Homework_13</h1>
+      <div isVisible [isvisible]="true">
+        <h2>Is Visible</h2>
+        <smart></smart>
+      </div>
+      <div makeBigger [bigger]="size">
+        <h2>Make Bigger</h2>
+        <p> Sample text to make it bigger.</p>
+      </div>
+      <button (click)="makeBigger()">Bigger</button>
+      <div>
+        <h1>Multi Pipe</h1>
+        <span>{{pipeString | multi: 5}}</span>
+      </div>
+    </div>
+    
   `,
+  //directives: [IsVisibleDirective],
   styleUrls: ['./app.component.css']
+  
 })
 export class AppComponent {
   title = 'angular';
+  pipeString = 'Hello';
+  size;
+  bigger = 25;
   counterValueOne = 5;
   counterValueTwo = 3;
   componentCounterValueOne;
@@ -32,5 +57,10 @@ export class AppComponent {
 
   onChangeTwo(value) {
     this.componentCounterValueTwo = value;
+  }
+
+  makeBigger() {
+    this.bigger += 2;
+    this.size = this.bigger + 'px';
   }
 }
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -4,12 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CounterOneComponent } from './Homework_12/counter1/counter.component';
 import { CounterTwoComponent } from './Homework_12/counter2/counter.component';
+import { SmartComponent } from './Homework_13/smart.component';
+import { DumbComponent } from './Homework_13/dumb.component';
+import { IsVisibleDirective } from './Homework_13/isVisible.directive';
+import { MakeBiggerDirective } from './Homework_13/makeBigger.directive';
+import { MultiPipes } from './Homework_13/multi.pipe';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CounterOneComponent,
-    CounterTwoComponent
+    CounterTwoComponent,
+    SmartComponent,
+    DumbComponent,
+    IsVisibleDirective,
+    MakeBiggerDirective,
+    MultiPipes
   ],
   imports: [
     BrowserModule
